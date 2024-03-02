7c08e51c23e62a563ae264282691877044bece5b
Andrew DGalga <ex.spake@gmail.com>
2024-03-01 19:57:51 -0600

    fragment test and init large images load. Performance terrible the way I'm doing it, todo fix.

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const imgBtnEvent = (imgName) => {
-    console.log(imgName);
+    //console.log(imgName);
 }
 
 window.onload = () =>{
+    const imgHost = document.getElementById('img-host');
+
     const imageBtns = document.getElementsByClassName('gallery-btn');
+    const imagePaths = [];
     
     for(let i = 0; i < imageBtns.length; i++){
         imageBtns[i].onclick = imgBtnEvent(imageBtns[i].dataset.fullscreen);
+        imagePaths.push(imageBtns[i].dataset.fullscreen);
     }
+
+    const fragment = document.createDocumentFragment();
+    imagePaths.forEach((imagePath)=>{
+        const img = document.createElement('img');
+        img.src = imagePath;
+        fragment.appendChild(img);
+    })
+
+    imgHost.appendChild(fragment);
 }
