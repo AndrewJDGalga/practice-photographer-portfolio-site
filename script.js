286f5bfe07753e3f7dcbaf7ed291bb855ee2018a
Andrew DGalga <ex.spake@gmail.com>
2024-03-07 00:05:22 -0600

    converted images, cleaned out old script.

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const imgHost = document.getElementById('img-host');
 
-const imgBtnEvent = (imgName) => {
-    console.log(imgName);
-    //imgHost.classList.remove('hide');
-}
-
 window.onload = () =>{
-    
     const carousel = new bootstrap.Carousel(imgHost);
 
     const carouselInner = document.getElementsByClassName('carousel-inner')[0];
@@ -17,39 +11,7 @@ window.onload = () =>{
     const imageBtns = document.getElementsByClassName('gallery-btn');
     for(let i = 0; i < imageBtns.length; i++){
         imageBtns[i].onclick = () => {
-            console.log('clicked');
             imgHost.classList.remove('hide');
-        }; //imgBtnEvent(imageBtns[i].dataset.fullscreen);
+        };
     }
-
-    /*
-    const imgHost = document.getElementById('img-host');
-
-    const imageBtns = document.getElementsByClassName('gallery-btn');
-    const imagePaths = [];
-    
-    for(let i = 0; i < imageBtns.length; i++){
-        imageBtns[i].onclick = imgBtnEvent(imageBtns[i].dataset.fullscreen);
-        imagePaths.push(imageBtns[i].dataset.fullscreen);
-    }
-
-    const fragment = document.createDocumentFragment();
-
-    const carouselHost = document.createElement('div');
-    fragment.appendChild(carouselHost);
-    
-    carouselHost.setAttribute('id', 'hiddenCarousel');
-    carouselHost.classList.add('carousel');
-    carouselHost.classList.add('slide');
-
-
-
-    imagePaths.forEach((imagePath)=>{
-        const img = document.createElement('img');
-        img.src = imagePath;
-        fragment.appendChild(img);
-    })
-
-    imgHost.appendChild(fragment);
-    */
 }
